453343e5484caabac840b84b415d529cc7cb13d0
Muhilan <mmuruga2@charlotte.edu>
2026-02-07 20:24:46 -0500

    feat: finished process_coins function which returns the total amount calculated from the coins inserted

--- main.py
+++ main.py
@@ -54,7 +54,12 @@ class SandwichMachine:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-
+        print("Please insert coins.")
+        total = int(input("how many dollars?: ")) * 1.0
+        total += int(input("how many half dollars?: ")) * 0.5
+        total += int(input("how many quarters?: ")) * 0.25
+        total += int(input("how many nickels?: ")) * 0.05
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
