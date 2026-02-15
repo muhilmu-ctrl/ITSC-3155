e34da6bd57a3d83815529933e882a5d8bdfee8b7
Muhilan <mmuruga2@charlotte.edu>
2026-02-15 13:39:05 -0500

    added instances of sandwich_maker and cashier, removed redundant code and instances, and updated instances in code.

--- main.py
+++ main.py
@@ -9,55 +9,10 @@ recipes = data.recipes
 sandwich_maker_instance = SandwichMaker(resources)
 cashier_instance = Cashier()
 
-### Data ###
-
-# recipes = {
-#     "small": {
-#         "ingredients": {
-#             "bread": 2,  ## slice
-#             "ham": 4,  ## slice
-#             "cheese": 4,  ## ounces
-#         },
-#         "cost": 1.75,
-#     },
-#     "medium": {
-#         "ingredients": {
-#             "bread": 4,  ## slice
-#             "ham": 6,  ## slice
-#             "cheese": 8,  ## ounces
-#         },
-#         "cost": 3.25,
-#     },
-#     "large": {
-#         "ingredients": {
-#             "bread": 6,  ## slice
-#             "ham": 8,  ## slice
-#             "cheese": 12,  ## ounces
-#         },
-#         "cost": 5.5,
-#     }
-# }
-#
-# resources = {
-#     "bread": 12,  ## slice
-#     "ham": 18,  ## slice
-#     "cheese": 24,  ## ounces
-# }
 
 
 ### Complete functions ###
 
-# class SandwichMachine:
-#
-#     def __init__(self, machine_resources):
-#         """Receives resources as input.
-#            Hint: bind input variable to self variable"""
-#         self.machine_resources = machine_resources
-
-
-### Make an instance of SandwichMachine class and write the rest of the codes ###
-
-# machine = SandwichMachine(resources)
 is_on = True
 
 while (is_on):
