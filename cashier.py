df42b1eae0da7b4b5f7d308188e55a79fdd06094
Muhilan <mmuruga2@charlotte.edu>
2026-02-15 13:27:58 -0500

    added process_coins and trasnaction_result methods to cashier.py

--- cashier.py
+++ cashier.py
@@ -5,9 +5,20 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        total = int(input("how many dollars?: ")) * 1.0
+        total += int(input("how many half dollars?: ")) * 0.5
+        total += int(input("how many quarters?: ")) * 0.25
+        total += int(input("how many nickels?: ")) * 0.05
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if (coins >= cost):
+            change = round(coins - cost, 2)
+            print("Here is $" + str(change) + " in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
