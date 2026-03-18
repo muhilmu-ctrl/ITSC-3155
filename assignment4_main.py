a92efa73b0fb7c7834983c773d5be0c51b520ca9
Muhilan <mmuruga2@charlotte.edu>
2026-03-17 23:21:55 -0400

    added code for Assignment4 part 2

--- assignment4_main.py
+++ assignment4_main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
