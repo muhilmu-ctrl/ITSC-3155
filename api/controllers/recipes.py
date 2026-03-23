3d09763e197adfa21c1102ed28e49c5168c4c84a
Muhilan <mmuruga2@charlotte.edu>
2026-03-22 22:50:38 -0400

    added recipes endpoints in main.py

--- api/controllers/recipes.py
+++ api/controllers/recipes.py
@@ -17,18 +17,18 @@ def create(db: Session, recipe: schemas.RecipeCreate):
 def read_all(db: Session):
     return db.query(models.Recipe).all()
 
-def read_one(db: Session, db_recipe):
-    return db.query(models.Recipe).filter(models.Recipe.id == db_recipe).first()
+def read_one(db: Session, recipe_id):
+    return db.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
 
-def update(db: Session, db_recipe: int, recipe: schemas.RecipeUpdate):
-    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == db_recipe)
+def update(db: Session, recipe_id: int, recipe: schemas.RecipeUpdate):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
     update_data = recipe.model_dump(exclude_unset=True)
     db_recipe.update(update_data, synchronize_session=False)
     db.commit()
     return db_recipe.first()
 
-def delete(db: Session, db_recipe: int):
-    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == db_recipe)
+def delete(db: Session, recipe_id: int):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
     db_recipe.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
